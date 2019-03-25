b7dc5c29330958603944f55feb6e53551528e1e8
jensc <jenschrj97@gmail.com>
2019-03-25 11:22:09 +0100

    brøk pis

--- assignment1.c
+++ assignment1.c
@@ -1,3 +1,17 @@
 // First Assignment
 
-a = 1;
+int main(int argc, const char* argv[]){
+
+	double pi = 0;
+	double brøk = 1;
+
+	for( int i=0; i< 10000; i++){
+
+		pi = pi + ((1/brøk) - (1/(brøk+2)));
+		brøk = brøk + 4;
+
+	}
+
+	pi = pi*4;
+	printf("%.6f",pi);
+}
